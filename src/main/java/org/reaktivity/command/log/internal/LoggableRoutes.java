284f65a0bc441edfa2148146889a52ec300a2aac
David Witherspoon <david.witherspoon@kaazing.com>
2018-08-29 12:53:56 -0700

    field ordering for json routes is "store", "hostname", "applicationProtocol"

--- src/main/java/org/reaktivity/command/log/internal/LoggableRoutes.java
+++ src/main/java/org/reaktivity/command/log/internal/LoggableRoutes.java
@@ -162,9 +162,10 @@ public final class LoggableRoutes implements AutoCloseable
             "\"hostname\":\"%s\"," +
             "\"applicationProtocol\":\"%s\"" +
             "}",
+            store,
             hostname,
-            applicationProtocol,
-            store);
+            applicationProtocol
+            );
         }
         return extension;
     }
